198de749352f06b28f4cabe7a042ceb447640bdd
Andrzej Ludwikowski <andrzej.ludwikowski@softwaremill.com>
2016-11-22 09:08:14 +0100

    tests

--- avro-serialization/src/test/java/schema/AvroSchemaTest.java
+++ avro-serialization/src/test/java/schema/AvroSchemaTest.java
@@ -2,9 +2,13 @@ package schema;
 
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
+import org.apache.log4j.Logger;
 import org.junit.Test;
+import wrapper.AvroGenericSenderTest;
 
 public class AvroSchemaTest {
+
+    private static final Logger LOG = Logger.getLogger(AvroGenericSenderTest.class);
 
     @Test
     public void shouldCreateStandardSchema() {
@@ -21,34 +25,11 @@ public class AvroSchemaTest {
                 .endRecord();
 
         //then
-        System.out.println(schema.toString());
+        LOG.info(schema.toString());
     }
 
     @Test
     public void shouldCreateSchemaWithUnion() {
-        //given
-        Schema smsSchema = SchemaBuilder
-                .record("Sms").namespace("com.softwaremill.schema")
-                .fields()
-                .name("phoneNumber").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord();
-
-        Schema emailSchema = SchemaBuilder
-                .record("Email").namespace("com.softwaremill.schema")
-                .fields()
-                .name("addressTo").type().stringType().noDefault()
-                .name("title").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord();
-
-        Schema pnSchema = SchemaBuilder
-                .record("PushNotification").namespace("com.softwaremill.schema")
-                .fields()
-                .name("arn").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord();
-
         //when
         Schema schema = SchemaBuilder
                 .record("MessageToSend").namespace("com.softwaremill.schema")
@@ -78,38 +59,6 @@ public class AvroSchemaTest {
                 .endRecord();
 
         //then
-        System.out.println(schema.toString());
-
-        //when
-        Schema schemaV2 = SchemaBuilder
-                .record("MessageToSendV2").namespace("com.softwaremill.schema")
-                .fields()
-                .name("text").type().stringType().noDefault()
-                .name("correlationId").type().stringType().noDefault()
-                .name("payload").type().unionOf()
-                .record("Sms").namespace("com.softwaremill.schema")
-                .fields()
-                .name("phoneNumber").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord().and()
-                .record("Email").namespace("com.softwaremill.schema")
-                .fields()
-                .name("addressFrom").type().stringType().stringDefault("andrzej@test.pl")
-                .name("addressTo").type().stringType().noDefault()
-                .name("title").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord()
-                .and()
-                .record("PushNotification").namespace("com.softwaremill.schema")
-                .fields()
-                .name("arn").type().stringType().noDefault()
-                .name("text").type().stringType().noDefault()
-                .endRecord()
-                .endUnion()
-                .noDefault()
-                .endRecord();
-
-        //then
-        System.out.println(schemaV2.toString());
+        LOG.info(schema.toString());
     }
 }
